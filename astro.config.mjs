f5292840db3c0af28e573ca87f0db503c1ee0975
suhasrk <suhasrk233@gmail.com>
2026-02-07 18:34:53 +0530

    Add Angular integration and update dependencies in configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import analogjsangular from "@analogjs/astro-angular";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: "https://suhasrk.github.io",
   base: "/portFolio",
+  integrations: [analogjsangular()],
 });
